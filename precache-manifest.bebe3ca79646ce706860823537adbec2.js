66c3acac2a0f056342ae5c26a9d46ae24962727f
Jayce <1198009679@qq.com>
2019-12-18 00:16:13 +0800

    Updates

---
+++ precache-manifest.bebe3ca79646ce706860823537adbec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b0b2c464deee1a8a5a8d6ad67cd783",
+    "url": "./index.html"
+  },
+  {
+    "revision": "229319d5a37b3cf9400d",
+    "url": "./static/css/2.fe483b9d.chunk.css"
+  },
+  {
+    "revision": "3a380dcbc6db8ff33d54",
+    "url": "./static/css/main.eefb6023.chunk.css"
+  },
+  {
+    "revision": "229319d5a37b3cf9400d",
+    "url": "./static/js/2.716af10a.chunk.js"
+  },
+  {
+    "revision": "3a380dcbc6db8ff33d54",
+    "url": "./static/js/main.de4222ed.chunk.js"
+  },
+  {
+    "revision": "29fe7f169f5aecd98872",
+    "url": "./static/js/runtime-main.6f1b9114.js"
+  },
+  {
+    "revision": "9aced879439fbace58b72fb9ebd25e80",
+    "url": "./static/media/avatar.9aced879.jpg"
+  }
+]);
